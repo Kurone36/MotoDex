e4702148038f434d05388aa4e5999d098f7da786
Kurone36 <58810589+Kurone36@users.noreply.github.com>
2020-09-29 20:21:11 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once "mail.php";  
+    if(isset( $_POST['name']))
+    $name = $_POST['name'];
+    if(isset( $_POST['surname']))
+    $surname = $_POST['surname'];
+    if(isset( $_POST['email']))
+    $email = $_POST['email'];
+    if(isset( $_POST['message']))
+    $message = $_POST['message'];
+    if(isset( $_POST['subject']))
+    $subject = $_POST['subject'];
+    if(isset( $_POST['marka']))
+    $marka = $_POST['marka'];
+    if(isset( $_POST['model']))
+    $model = $_POST['model'];
+    if(isset( $_POST['rocznik']))
+    $rocznik = $_POST['rocznik'];
+
+ // Multiple recipients
+$to = 'johny@example.com, sally@example.com'; // note the comma
+
+// Subject
+$subject = 'Birthday Reminders for August';
+
+// Message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here łążźćąęare the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+// Additional headers
+$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+$headers[] = 'From: Birthday Reminder <birthday@example.com>';
+$headers[] = 'Cc: birthdayarchive@example.com';
+$headers[] = 'Bcc: birthdaycheck@example.com';
+
+// Mail it
+mail($to, $subject, $message, implode("\r\n", $headers));
+
+?>
